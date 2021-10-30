3e48c7c5670f349904baf672e5850491ff632601
YouGotMyBluePixel <xaverericketts@gmail.com>
2021-10-29 23:46:46 -0500

    download pdf functionality added , updated buttons on table

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,14 +12,60 @@
   </header>
   <main>
     
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div 
+    class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
           <button
       id="button"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      class="w-14 h-auto  px-4 py-3 addPointBox rounded hover:bg-white font-semibold transition duration-200 each-in-out"
       @click="showModal()"
     >
-      ADD
+     <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke=""
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-edit"
+                          >
+                           <path fill="none" d="M13.388,9.624h-3.011v-3.01c0-0.208-0.168-0.377-0.376-0.377S9.624,6.405,9.624,6.613v3.01H6.613c-0.208,0-0.376,0.168-0.376,
+                           0.376s0.168,0.376,0.376,0.376h3.011v3.01c0,0.208,0.168,0.378,0.376,
+                           0.378s0.376-0.17,0.376-0.378v-3.01h3.011c0.207,0,0.377-0.168,0.377-0.376S13.595,9.624,13.388,
+                           9.624z M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,
+                           5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z"></path>
+                          </svg>
+      
+    </button>
+     <button
+     @click="makePDF"
+      type="button"
+   class="w-14 h-auto  px-4 py-3 editPointBox rounded hover:bg-white font-semibold transition duration-200 each-in-out"
+    >
+    
+      <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke=""
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-edit"
+                          >
+                           <path fill="none" d="M15.608,6.262h-2.338v0.935h2.338c0.516,0,0.934,0.418,0.934,0.935v8.879c0,0.517-0.418,0.935-0.934,0.935H4.392c-0.516,
+                           0-0.935-0.418-0.935-0.935V8.131c0-0.516,0.419-0.935,0.935-0.935h2.336V6.262H4.392c-1.032,
+                           0-1.869,0.837-1.869,1.869v8.879c0,1.031,0.837,1.869,1.869,1.869h11.216c1.031,0,1.869-0.838,1.869-1.869V8.131C17.478,7.099,
+                           16.64,6.262,15.608,6.262z M9.513,11.973c0.017,0.082,0.047,0.162,0.109,0.226c0.104,0.106,0.243,0.143,0.378,0.126c0.135,0.017,
+                           0.274-0.02,0.377-0.126c0.064-0.065,0.097-0.147,0.115-0.231l1.708-1.751c0.178-0.183,0.178-0.479,0-0.662c-0.178-0.182-0.467-0.182-0.645,0l
+                           -1.101,1.129V1.588c0-0.258-0.204-0.467-0.456-0.467c-0.252,0-0.456,0.209-0.456,0.467v9.094L8.443,9.553c-0.178-0.182-0.467-0.182-0.645,0c-0.178,0.184-0
+                           .178,0.479,0,0.662L9.513,11.973z"></path>
+                          </svg>
     </button>
     <add-bus-route-modal
     v-show="isAddBusRouteModalVisible"
@@ -141,6 +187,12 @@ import $ from 'jquery';
 import AddBusRouteModal from './AddBusRouteModal.vue'
 import EditBusRouteModal from './EditBusRouteModal.vue'
 import DeleteBusRouteModal from './DeleteBusRouteModal.vue'
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable'
+import autoTable from 'jspdf-autotable'
+
+
 
 
 
@@ -163,6 +215,11 @@ export default {
         DeleteBusRouteModal,
     },
     methods: {
+      makePDF(){
+        const doc = new jsPDF()
+        autoTable(doc, { html: '#example' })
+        doc.save('BusRoutes.pdf')
+      },
         showModal(id) {
         this.isAddBusRouteModalVisible = true;
       },
@@ -245,6 +302,15 @@ export default {
 
 .editPointBox:hover {
   stroke: rgb(74, 159, 228);
+}
+.addPointBox {
+  border: 1px solid rgb(0, 140, 255);
+  background: rgb(0, 140, 255);
+  stroke: white;
+  margin: 5px;
+}
+.addPointBox:hover {
+  stroke: rgb(0, 140, 255);
 }
 .deletePointBox {
   border: 1px solid red;
